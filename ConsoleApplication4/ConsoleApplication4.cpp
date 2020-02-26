9a9b1b22c52fbedc5dbf8452b38b60cc38265b81
Nikita Nasedkin <n.nasedkin@gmail.com>
2020-02-26 17:00:16 +0300

    DFS and BFS path and count of peaks mapping

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -4,6 +4,14 @@
 #include <stack>
 #include <queue>
 using namespace std;
+void Show(vector<int> path)
+{
+	for (int i = 0; i < path.size(); i++)
+	{
+		cout << path[i] << ", ";
+	}
+	cout << endl;
+}
 void ShowLst(vector<vector<int>> list)
 {
 	for (int i = 0; i < list.size(); i++)
@@ -41,9 +49,11 @@ vector<vector<bool>> Convert(vector<vector<int>> list)
 }
 void dfs(vector <vector<int>> list, vector <vector<bool>> visited, int v)
 {
+	vector<int> path;
 	int count = 1;
 	stack<int> plan;
 	visited[v][v] = 1;
+	path.push_back(v);
 	for (int i = 0; i< list[v].size(); i++)
 		plan.push(list[v][i]);
 	while (!plan.empty())
@@ -51,6 +61,7 @@ void dfs(vector <vector<int>> list, vector <vector<bool>> visited, int v)
 		if (visited[plan.top()][plan.top()] == 0)
 		{
 			visited[plan.top()][plan.top()] = 1;
+			path.push_back(plan.top());
 			count++;
 			int current = plan.top();
 			plan.pop();
@@ -60,14 +71,17 @@ void dfs(vector <vector<int>> list, vector <vector<bool>> visited, int v)
 		else
 			plan.pop();
 	}
-	ShowMtrx(visited);
-	cout << count << endl;
+	cout << "DFS path: ";
+	Show(path);
+	cout <<"Count of peaks: "<< count << endl;
 }
 void bfs(vector <vector<int>> list, vector <vector<bool>> visited, int v)
 {
+	vector<int> path;
 	int count = 1;
 	queue<int> plan;
 	visited[v][v] = 1;
+	path.push_back(v);
 	for (int i = 0; i < list[v].size(); i++)
 		plan.push(list[v][i]);
 	while (!plan.empty())
@@ -75,6 +89,7 @@ void bfs(vector <vector<int>> list, vector <vector<bool>> visited, int v)
 		if (visited[plan.front()][plan.front()] == 0)
 		{
 			visited[plan.front()][plan.front()] = 1;
+			path.push_back(plan.front());
 			count++;
 			int current = plan.front();
 			plan.pop();
@@ -84,8 +99,9 @@ void bfs(vector <vector<int>> list, vector <vector<bool>> visited, int v)
 		else
 			plan.pop();
 	}
-	ShowMtrx(visited);
-	cout << count << endl;
+	cout << "BFS path: ";
+	Show(path);
+	cout << "Count of peaks: " << count << endl;
 
 }
 int main()
@@ -96,10 +112,11 @@ int main()
 	lst[2] = { 1 };
 	lst[3] = { 0,4 };
 	lst[4] = { 0,1,3 };
-	ShowLst(lst);
+	/*ShowLst(lst);
 	cout << endl;
-	ShowMtrx(Convert(lst));
+	ShowMtrx(Convert(lst));*/
 	dfs(lst, Convert(lst), 1);
+	cout << endl;
 	bfs(lst, Convert(lst), 3);
 	return 0;
 }
